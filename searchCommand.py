cbe597b18e0e3b332121a5f5c3e85a857e820289
SkyW4r33x <31325020+SkyW4r33x@users.noreply.github.com>
2025-06-02 19:59:17 -0500

    searchCommand.py

--- searchCommand.py
+++ searchCommand.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+# ADFASDF SADF ASD
 """
 ═════════════════════════[searchCommand]════════════════════════════
 Autor       : Jordan Cueva Mendoza (aka SkyW4r33x)
