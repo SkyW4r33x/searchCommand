dd6dab356fa4ac2ff10e649586e7d14a08a33bd3
SkyW4r33x <31325020+SkyW4r33x@users.noreply.github.com>
2025-06-03 14:09:47 -0500

    searchCommand.py

--- searchCommand.py
+++ searchCommand.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# test
+
 """
 ═════════════════════════[searchCommand]════════════════════════════
 Autor       : Jordan Cueva Mendoza (aka SkyW4r33x)
@@ -41,7 +41,7 @@ from fuzzywuzzy import process
 
 warnings.filterwarnings("ignore", category=Warning, module="urllib3")
 
-__version__ = "1.3"
+__version__ = "1.5"
 
 if os.name == 'nt':
     print(f"{Colors.RED}{Colors.BOLD}[-]{Colors.RESET} Este programa está diseñado para Linux/macOS. En Windows, usa WSL para mejor compatibilidad.")
