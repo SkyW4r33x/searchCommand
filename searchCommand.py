39129bf45c200829addd1b74a5d879d2a9184232
SkyW4r33x <31325020+SkyW4r33x@users.noreply.github.com>
2025-06-08 12:20:04 -0500

    searchCommand.py

--- searchCommand.py
+++ searchCommand.py
@@ -53,7 +53,7 @@ from fuzzywuzzy import process
 
 warnings.filterwarnings("ignore", category=InsecureRequestWarning, module="urllib3")
 
-__version__ = "2.7.2"
+__version__ = "2.8.2"
 
 if os.name == 'nt':
     print(f"{Colors.RED}{Colors.BOLD}[-]{Colors.RESET} Este programa está diseñado para Linux/macOS. En Windows, usa WSL para mejor compatibilidad.")
